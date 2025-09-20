bc510774ee0218ea8e43a1d0bb2c5121f0f9726e
rosjerry <rostomi1306@gmail.com>
2025-09-21 00:46:58 +0400

    chore: use main.py for several python trick learn

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+def main():
+    print(calculate())
+    hello()
+
+def square(num):
+    return num * num
+
+def calculate():
+    return 2 + 2 != 5 and (3 * 3 != 10 and 5 / 5 == 1)
+
+def hello(name="john doe"):
+    print(f"Hello {name}, my friend!")
+
+main()
