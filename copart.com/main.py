b1ca3819427ab440bba1b745e04980cbf9446519
rosjerry <rostomi1306@gmail.com>
2025-09-23 18:40:33 +0400

    chore: add pseudo code for copart scraper

---
+++ copart.com/main.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import Page
+
+def main ():
+  print("copart")
+
+# go to copart vehicle finder url
+# type in make, model, years, damage type
+# click search
+# change pagination to maximum
+# scrape all urls
+# check if this url already exists in db
+# scrape all gathered urls after one ecahother
+# download all images into respective folders outside the project
+# make additional txt file for each folder, including url, title and etc.
+# push parsed link into db
+  
+if __name__ == "__main__":
+  main()
+  
