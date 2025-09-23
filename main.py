85a09b2df6887fdf91978e5120c5772a53be13de
rosjerry <rostomi1306@gmail.com>
2025-09-23 17:20:22 +0400

    chore: learn exceptions, install lib cowsay, use imports

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-import tensorflow as tf; 
-
-print(tf.config.list_physical_devices('GPU'))
-
+# import tensorflow as tf
+from random import choice, randint, shuffle
+from statistics import mean
+import sys
+from cowsay import cow, trex
+    
 def main():
-    print(tf.config.list_physical_devices('GPU'))
+    # random()
+    # stats()
+    try:
+        cow("hello")
+        trex("world")
+    except:
+        print("error happ")
+
+
+def random():
+    nums = ["1", "2", "6", "5"]
+    shuffle(nums)
+    for c in nums:
+        print(c)
+
+def stats():
+    mnn = mean([100, 80, 56, 12])
+    print(mnn)
+
 
 main()
