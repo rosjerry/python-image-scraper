6c8e15a456f8c24b08170682ce1a2ba2cbf7a3a1
rosjerry <rostomi1306@gmail.com>
2025-09-23 20:44:41 +0400

    feat: change pseudocode, add captcha check, create docker compose

--- copart.com/main.py
+++ copart.com/main.py
@@ -1,11 +1,23 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
-def main ():
+# url query of results from copart
+url = 'https://www.copart.com/lotSearchResults?free=false&searchCriteria=%7B%22query%22:%5B%22*%22%5D,%22filter%22:%7B%22PRID%22:%5B%22damage_type_code:DAMAGECODE_NW%22%5D,%22ODM%22:%5B%22odometer_reading_received:%5B0%20TO%209999999%5D%22%5D,%22YEAR%22:%5B%22lot_year:%5B2014%20TO%202017%5D%22%5D,%22MISC%22:%5B%22%23VehicleTypeCode:VEHTYPE_V%22,%22%23MakeCode:HOND%20OR%20%23MakeDesc:Honda%22,%22%23LotModel:%5C%22ACCORD%5C%22%22%5D%7D,%22searchName%22:%22%22,%22watchListOnly%22:false,%22freeFormSearch%22:false%7D%20&displayStr=AUTOMOBILE,NORMAL%20WEAR,%5B0%20TO%209999999%5D,%5B2014%20TO%202017%5D,Honda,Accord&from=%2FvehicleFinder&fromSource=widget&qId=b121218d-a368-4941-a95c-ab37b276580e-1758645680911'
+
+def test_main ():
   print("copart")
+  
 
 # go to copart vehicle finder url
-# type in make, model, years, damage type
-# click search
+def test_visit_website(page: Page):
+  page.goto(url)
+  security_check_locator = page.locator("p:has-text('Additional security check is required')")
+  visible = security_check_locator.is_visible()
+
+  if visible:
+    print("continue is unavailable at this moment, try again later")
+  else:
+    pass
+
 # change pagination to maximum
 # scrape all urls
 # check if this url already exists in db
@@ -15,5 +27,4 @@ def main ():
 # push parsed link into db
   
 if __name__ == "__main__":
-  main()
-  
+  test_main()
--- main.py
+++ main.py
@@ -3,26 +3,10 @@ from random import choice, randint, shuffle
 from statistics import mean
 import sys
 from cowsay import cow, trex
+from requests import get
     
 def main():
-    # random()
-    # stats()
-    try:
-        cow("hello")
-        trex("world")
-    except:
-        print("error happ")
-
-
-def random():
-    nums = ["1", "2", "6", "5"]
-    shuffle(nums)
-    for c in nums:
-        print(c)
-
-def stats():
-    mnn = mean([100, 80, 56, 12])
-    print(mnn)
+    print("asdfg")
 
 
 main()
