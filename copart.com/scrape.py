0e4bba45c5b2950cd2784a23bafd17b64b3c8bcc
rosjerry <rostomi1306@gmail.com>
2025-09-23 23:03:45 +0400

    chore: format repo

--- copart.com/scrape.py
+++ copart.com/scrape.py
@@ -1,42 +1,50 @@
 from playwright.sync_api import sync_playwright, Playwright
 
-
 # url query of results from copart
-url = 'lotSearchResults?free=false&searchCriteria=%7B%22query%22:%5B%22*%22%5D,%22filter%22:%7B%22PRID%22:%5B%22damage_type_code:DAMAGECODE_NW%22%5D,%22ODM%22:%5B%22odometer_reading_received:%5B0%20TO%209999999%5D%22%5D,%22YEAR%22:%5B%22lot_year:%5B2014%20TO%202017%5D%22%5D,%22MISC%22:%5B%22%23VehicleTypeCode:VEHTYPE_V%22,%22%23MakeCode:HOND%20OR%20%23MakeDesc:Honda%22,%22%23LotModel:%5C%22ACCORD%5C%22%22%5D%7D,%22searchName%22:%22%22,%22watchListOnly%22:false,%22freeFormSearch%22:false%7D%20&displayStr=AUTOMOBILE,NORMAL%20WEAR,%5B0%20TO%209999999%5D,%5B2014%20TO%202017%5D,Honda,Accord&from=%2FvehicleFinder&fromSource=widget&qId=b121218d-a368-4941-a95c-ab37b276580e-1758645680911'
+url = "lotSearchResults?free=false&searchCriteria=%7B%22query%22:%5B%22*%22%5D,%22filter%22:%7B%22PRID%22:%5B%22damage_type_code:DAMAGECODE_NW%22%5D,%22ODM%22:%5B%22odometer_reading_received:%5B0%20TO%209999999%5D%22%5D,%22YEAR%22:%5B%22lot_year:%5B2014%20TO%202017%5D%22%5D,%22MISC%22:%5B%22%23VehicleTypeCode:VEHTYPE_V%22,%22%23MakeCode:HOND%20OR%20%23MakeDesc:Honda%22,%22%23LotModel:%5C%22ACCORD%5C%22%22%5D%7D,%22searchName%22:%22%22,%22watchListOnly%22:false,%22freeFormSearch%22:false%7D%20&displayStr=AUTOMOBILE,NORMAL%20WEAR,%5B0%20TO%209999999%5D,%5B2014%20TO%202017%5D,Honda,Accord&from=%2FvehicleFinder&fromSource=widget&qId=b121218d-a368-4941-a95c-ab37b276580e-1758645680911"
 base_url = "https://www.copart.com/"
 
 
 def run(pw: Playwright):
     chromium = pw.chromium
-    launch_chromium = chromium.launch(
-        headless=False
-    )
+    launch_chromium = chromium.launch(headless=False)
     page = launch_chromium.new_page(
         accept_downloads=True, base_url=base_url, bypass_csp=True
     )
-    
+
     # go to url
-    page.goto(url=url, timeout=0) 
-    
+    page.goto(url=url, timeout=0)
+
     # Find the span with class 'blue-heading' and get its text content
     counted_search_result = page.locator("tbody.p-datatable-tbody")
     tr_elements = counted_search_result.locator("tr")
     quantity = tr_elements.count()
-    
-    if(quantity <= 20):
+
+    if quantity <= 20:
         pass
     else:
-        paginator_dropdown = page.locator("div.p-paginator-rpp-options.p-dropdown.p-component.p-inputwrapper.p-inputwrapper-filled")
+        paginator_dropdown = page.locator(
+            "div.p-paginator-rpp-options.p-dropdown.p-component.p-inputwrapper.p-inputwrapper-filled"
+        )
         paginator_dropdown.click()
-        
+
         for _ in range(4):
             page.keyboard.press("ArrowDown")
         page.keyboard.press("Enter")
-    
+
     print(f"Number of <tr> elements: {quantity}")
     
-    input("Press Enter to close browser...")
+    urls = []
+    for i in range(quantity):
+        tr = tr_elements.nth(i)
+        second_td = tr.locator("td").nth(1)
+        a_element = second_td.locator("a").first
+        href = a_element.get_attribute("href")
+        if href:
+            urls.append(f"{base_url}/{href}")
+    print(urls)
 
+    input("Press Enter to close browser...")
 
 
 # scrape all urls
